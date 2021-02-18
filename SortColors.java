61e4de0f574a7f61e6eaf9a315d918245c429e7e
Shreyans <shreyansmittal.14@gmail.com>
2021-02-18 14:23:09 +0530

    Create SortColors.java

---
+++ SortColors.java
@@ -0,0 +1,34 @@
+/*
+https://leetcode.com/problems/sort-colors/
+*/
+class SortColors {
+    public void sortColors(int[] nums) {
+        
+        int zeroIndex = 0;
+        int twoIndex = nums.length-1;
+        int currentIndex = 0;
+        
+        while(currentIndex<=twoIndex){
+            
+            if(nums[currentIndex]==0){
+                swapIndexes(nums,currentIndex,zeroIndex);
+                zeroIndex++;
+                currentIndex++;
+            }
+            else if(nums[currentIndex]==2){
+                swapIndexes(nums,currentIndex,twoIndex);
+                twoIndex--;
+            }            
+            else if(nums[currentIndex]==1)
+                currentIndex++;
+            
+        }
+    }
+    
+    private void swapIndexes(int[] nums, int index1, int index2){
+        int temp = nums[index1];
+        nums[index1] = nums[index2];
+        nums[index2] = temp;
+        return;
+    }
+}
